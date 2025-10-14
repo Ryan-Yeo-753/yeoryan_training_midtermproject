15cc6b7f712aeead1cfa0bf9a4634d7ca0c109ac
Ryan_Yeo_753 <stmallworldforever@gmail.com>
2025-10-14 16:54:42 -0700

    Added login & create account buttons, added all pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,69 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 void main() {
   runApp(const MyApp());
 }
+
+final GoRouter _router = GoRouter(
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (BuildContext context, GoRouterState state) {
+        return MyHomePage();
+      },
+      routes: [
+        GoRoute(
+          path: 'create_account',
+          builder: (BuildContext context, GoRouterState state) {
+            return CreateAccount();
+          },
+        ),
+        GoRoute(
+          path: 'login',
+          builder: (BuildContext context, GoRouterState state) {
+            return LogIn();
+          },
+        ),
+        GoRoute(
+          path: 'dashboard',
+          builder: (BuildContext context, GoRouterState state) {
+            return Dashboard();
+          },
+        ),
+        GoRoute(
+          path: 'deposit',
+          builder: (BuildContext context, GoRouterState state) {
+            return Deposit();
+          },
+        ),
+        GoRoute(
+          path: 'withdraw',
+          builder: (BuildContext context, GoRouterState state) {
+            return Withdraw();
+          },
+        ),
+        GoRoute(
+          path: 'loan',
+          builder: (BuildContext context, GoRouterState state) {
+            return Loan();
+          },
+        ),
+      ],
+    ),
+  ],
+);
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(),
+      routerConfig: _router,
     );
   }
 }
@@ -33,8 +83,145 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            ElevatedButton(onPressed: () => print('Hello, Flutter Training!'), child: Text('Print!')),
+            Container(
+              color: Colors.blueGrey,
+              width: 400,
+              height: 450,
+              child: Stack(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.all(15),
+                    child: Text('Welcome to the bank!', style: TextStyle()),
+                  ),
+                ],
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () => context.push('/login'),
+              style: ElevatedButton.styleFrom(
+                foregroundColor: Colors.black,
+                backgroundColor: Colors.orangeAccent,
+                padding: EdgeInsets.all(20),
+                fixedSize: Size(250, 50),
+                textStyle: TextStyle(fontWeight: FontWeight.bold),
+                side: BorderSide(color: Colors.black, width: 2),
+                shape: StadiumBorder(),
+              ),
+              child: Text('Log In'),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () => context.push('/create_account'),
+              style: ElevatedButton.styleFrom(
+                foregroundColor: Colors.black,
+                backgroundColor: Colors.orangeAccent,
+                padding: EdgeInsets.all(20),
+                fixedSize: Size(250, 50),
+                textStyle: TextStyle(fontWeight: FontWeight.bold),
+                side: BorderSide(color: Colors.black, width: 2),
+                shape: StadiumBorder(),
+              ),
+              child: Text('Create an Account'),
+            ),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class CreateAccount extends StatelessWidget {
+  const CreateAccount({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Create an Account')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [],
+        ),
+      ),
+    );
+  }
+}
+
+class LogIn extends StatelessWidget {
+  const LogIn({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [TextField()],
+        ),
+      ),
+    );
+  }
+}
+
+class Dashboard extends StatelessWidget {
+  const Dashboard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [TextField()],
+        ),
+      ),
+    );
+  }
+}
+
+class Deposit extends StatelessWidget {
+  const Deposit({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [TextField()],
+        ),
+      ),
+    );
+  }
+}
+
+class Withdraw extends StatelessWidget {
+  const Withdraw({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [TextField()],
+        ),
+      ),
+    );
+  }
+}
+
+class Loan extends StatelessWidget {
+  const Loan({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [TextField()],
         ),
       ),
     );
